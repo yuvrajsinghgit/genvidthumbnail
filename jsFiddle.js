421e934e8ee2bfbd784bb0795a7df96f0c0ac4a3
yuvrajsinghgit <98418267+yuvrajsinghgit@users.noreply.github.com>
2023-05-25 16:24:22 +0530

    Delete jsFiddle.js

--- jsFiddle.js
+++
@@ -1,1 +0,0 @@
-function yes() { var button = document.getElementById('test'); var name = document.getElementById('name'); var age = document.getElementById('age'); var location = document.getElementById('location'); var str = 'Hello ' + name.value + ', you are ' + age.value + ' years old and from ' + location.value; document.getElementById('test').innerHTML=str; };
